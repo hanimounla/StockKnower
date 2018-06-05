20c037c6edee64d14e29ffc06ed502425ccb86c1
hani-_000 <hani-mounla@live.com>
2018-06-05 10:08:35 +0300

    getting stock data

---
+++ Get_stock_Data.py
@@ -0,0 +1,50 @@
+# Make sure that you have all these libaries available to run the code successfully
+from pandas_datareader import data
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+import urllib.request, json
+import os
+import numpy as np
+import tensorflow as tf # This code has been tested with TensorFlow 1.6
+from sklearn.preprocessing import MinMaxScaler
+
+
+#my api_key, You can get yours from alphavantage
+api_key = '3GL2O8TQS330GIZR'
+
+# SAP stock market prices
+ticker = "AAPL"
+
+# JSON file with all the stock market data for SAP from the last 20 years
+url_string = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=%s&outputsize=full&apikey=%s"%(ticker,api_key)
+
+
+date = dt.datetime.now().strftime("%y.%m.%d")
+full_name = ticker + "-" + date
+print(full_name)
+# Save data to this file
+file_to_save = 'Data\\%s.csv'%full_name
+
+# If you haven't already saved data,
+# Go ahead and grab the data from the url
+# And store date, low, high, volume, close, open values to a Pandas DataFrame
+if not os.path.exists(file_to_save):
+    with urllib.request.urlopen(url_string) as url:
+        data = json.loads(url.read().decode())
+        # extract stock market data
+        data = data['Time Series (Daily)']
+        df = pd.DataFrame(columns=['Date','Low','High','Close','Open'])
+        for k,v in data.items():
+            date = dt.datetime.strptime(k, '%Y-%m-%d')
+            data_row = [date.date(),float(v['3. low']),float(v['2. high']),
+                        float(v['4. close']),float(v['1. open'])]
+            df.loc[-1,:] = data_row
+            df.index = df.index + 1
+    print('Data saved to : %s'%file_to_save)        
+    df.to_csv(file_to_save)
+
+# If the data is already there, just load it from the CSV
+else:
+    print('File already exists. Loading data from CSV')
+    df = pd.read_csv(file_to_save)
